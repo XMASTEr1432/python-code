cb9619bd41b08cde5afd54a4e6eb6b948b90ad80
XMASTEr1432 <54854992+XMASTEr1432@users.noreply.github.com>
2021-03-10 16:02:32 +0100

    Update flamingo.py

--- flamingo.py
+++ flamingo.py
@@ -1,12 +1,12 @@
-from tkinter import *
+from tkinter import * ## library that you need to use dor this to work
 a = Tk()
-a.geometry("400x400")
+a.geometry("400x400") ## window size
 a.resizable(width=False, height=False)
-a.configure(bg="pink", cursor="heart")
-#img = PhotoImage(file="flamingo.png")
+a.configure(bg="pink", cursor="heart") ## here you can change some thing e.g. background color or cursor style
+img = PhotoImage(file="flamingo.png") ## <-- here you need a flamingo image file named "flamingo.png"
 canvas = Canvas(a, width = 400, height = 400, bg = "pink",bd=0, highlightthickness=0,)      
 canvas.pack()
-#canvas.create_image(20,20,anchor=NW, image=img) 
-a.title("flamingo")
-#a.iconphoto(False, img)
-a.mainloop()
+canvas.create_image(20,20,anchor=NW, image=img) 
+a.title("flamingo") ## this changes the window title
+a.iconphoto(False, img)
+a.mainloop() ## you need this to have a window running
